941dad9db0cae279286660f2e4200e532f9c7287
IlyaGalyatdinov <i.galyatdinov@yandex.ru>
2023-12-05 12:13:26 +0500

    добавил смену слайдера по времени

--- src/js/modules/demonstrationSlider.js
+++ src/js/modules/demonstrationSlider.js
@@ -2,6 +2,10 @@ function demonstrationSlider() {
     const slider = document.querySelector('.demonstration__slider.swiper');
     if (slider) {
         const swiper = new Swiper(slider, {
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+              },
             pagination: {
                 el: ".swiper-pagination",
             },
